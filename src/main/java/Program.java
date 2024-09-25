e7d3fdd764c79cd2ec908dde520761f63a196cec
ADMIN <ADMIN@PC238>
2024-09-25 07:42:37 +0700

    LapTrinhGiaoDien

---
+++ src/main/java/Program.java
@@ -0,0 +1,18 @@
+
+import my.gui.JNotepad;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        JNotepad app= new JNotepad("Demo Notepad");
+        app.setVisible(true);
+    }
+}
